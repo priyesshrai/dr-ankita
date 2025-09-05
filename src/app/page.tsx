d7e857e189b123aa8ef5d8c2a48797a8349e05c6
priyesshrai <priyeshrai369@gmail.com>
2025-09-05 16:35:30 +0530

    Add footer component with social media icons and contact information
    - Created a new Footer component to be included in the main page.
    - Added SVG icons for social media platforms: Facebook, Google, Instagram, LinkedIn, YouTube, and X.
    - Included contact information with icons for address, phone, and email.
    - Implemented quick links section in the footer.
    - Integrated Google Maps iframe for location display.
    - Added logo image to the footer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import MarqueeStrip from "@/components/Marquee";
 import OtherFacilityes from "@/components/OtherFacilityes";
@@ -14,6 +15,7 @@ export default function Home() {
       <OtherFacilityes />
       <MarqueeStrip />
       <Contact />
+      <Footer />
     </main>
   );
 }
