835d2a7db191f42fad467b3689d22b488bd954ca
priyesshrai <priyeshrai369@gmail.com>
2026-01-10 11:12:35 +0530

    update form

--- src/components/BookAppointment.tsx
+++ src/components/BookAppointment.tsx
@@ -53,24 +53,29 @@ export default function BookAppointment({ openForm, closeForm }: FormProps) {
                     transition={{ duration: 0.3 }}
                 >
                     <div className='w-full relative h-max'>
-                        <motion.div className='max-w-xl w-full bg-white md:p-5 p-2.5 rounded-2xl mx-auto relative'
+                        <motion.div className='max-w-xl w-full bg-white rounded-2xl mx-auto relative overflow-hidden'
                             key="form"
                             initial={{ y: 500, opacity: 0, scale: 0.9 }}
                             animate={{ y: 0, opacity: 1, scale: 1 }}
-                            exit={{ y: -500, opacity: 0, scale: 0.9}}
+                            exit={{ y: -500, opacity: 0, scale: 0.9 }}
                             transition={{ duration: 0.35, ease: [0.25, 0.8, 0.5, 1] }}
                         >
-                            <div className='w-full flex items-center justify-between'>
-                                <Image src={'/images/logo/logo-2.svg'} alt='Dr G Nikila Reddy'
-                                    width={400} height={100} className='lg:w-[300px] md:w-[250px] w-[200px]'
-                                />
-                                <button role='button' className='md:w-11 md:h-11 h-9 w-9 rounded-full bg-zinc-900 text-white flex items-center justify-center cursor-pointer' onClick={() => closeForm(false)}>
+                            <div className='w-full flex items-center justify-between bg-primary-color md:p-5 p-2.5'>
+                                <div className='flex gap-1 items-center'>
+                                    <Image src={'/images/logo/new-logo-1.png'} alt='Dr Ankita'
+                                        width={400} height={100} className='w-10'
+                                    />
+                                    <Image src={'/images/logo/new-logo-2.png'} alt='Dr Ankita'
+                                        width={400} height={100} className='w-50'
+                                    />
+                                </div>
+                                <button role='button' className='shrink-0 md:w-11 md:h-11 h-9 w-9 rounded-full bg-zinc-900 text-white flex items-center justify-center cursor-pointer' onClick={() => closeForm(false)}>
                                     <X />
                                 </button>
                             </div>
 
                             <form>
-                                <div className='w-full relative mt-8 flex flex-col gap-5 text-zinc-700 font-montserrat font-medium text-lg'>
+                                <div className='w-full relative mt-8 flex flex-col gap-5 text-zinc-700 font-montserrat font-medium text-lg md:p-5 p-2.5'>
                                     <div className='relative w-full px-2 py-3 border border-primary-hover rounded-md bg-transparent '>
                                         <input type="text" name="name" id="name" className='relative w-full h-full border-none outline-none bg-transparent' placeholder='' />
                                         <label className='labels'>
